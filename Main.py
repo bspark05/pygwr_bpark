f7ebb7ba517bc9cf25c79dc3098e651b9e70423c
bspark05 <bpark21@asu.edu>
2015-11-11 19:27:45 -0700

    11/11/2015-1

--- Main.py
+++ Main.py
@@ -8,6 +8,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from array import array
 import numpy as np
+from pysal.esda.mapclassify import Fisher_Jenks
+from matplotlib.colors import Normalize
+
+def norm_cmap(values, cmap, vmin=None, vmax=None):
+    """
+    Normalize and set colormap
+    
+    Parameters
+    ----------
+    values : Series or array to be normalized
+    cmap : matplotlib Colormap
+    normalize : matplotlib.colors.Normalize
+    cm : matplotlib.cm
+    vmin : Minimum value of colormap. If None, uses min(values).
+    vmax : Maximum value of colormap. If None, uses max(values).
+    
+    Returns
+    -------
+    n_cmap : mapping of normalized values to colormap (cmap)
+    
+    """
+    mn = vmin or min(values)
+    mx = vmax or max(values)
+    norm = Normalize(vmin=mn, vmax=mx)
+    n_cmap = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+    return n_cmap
 
 
 if __name__ == '__main__':
@@ -36,7 +62,15 @@ if __name__ == '__main__':
 #     cmap = sns.cubehelix_palette(light=1, as_cmap=True)
 #     sns.kdeplot(xCoordArr, yCoordArr, n_levels=30, cmap="Purples_d");
     
-    plt.scatter(xCoord,yCoord)
+    attr1Arr = np.asarray(attr1)
+    fj = Fisher_Jenks(attr1Arr, k=6)
+    cmap = norm_cmap(fj.yb, cmap='YlGn')
+    cmap1 = [cmap.to_rgba(value) for value in attr1Arr]
+    
+    print(cmap)
+    print(cmap1)  
+    
+    plt.scatter(xCoord,yCoord, c=cmap1)
     plt.show()
 
     
